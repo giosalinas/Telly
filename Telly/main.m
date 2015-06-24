8a18be7f7eba1bdad7809414dc56e81f76d07666
Gio Salinas <geovanne.salinas@safecard.cl>
2015-06-24 16:33:41 -0300

    Initial Commit

---
+++ Telly/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Telly
+//
+//  Created by Gio Salinas on 24-06-15.
+//  Copyright (c) 2015 Gio Salinas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ TellyTests/TellyTests.m
@@ -0,0 +1,40 @@
+//
+//  TellyTests.m
+//  TellyTests
+//
+//  Created by Gio Salinas on 24-06-15.
+//  Copyright (c) 2015 Gio Salinas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TellyTests : XCTestCase
+
+@end
+
+@implementation TellyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
